ed07a48891c9575be12eea3b974adc4cc4536af2
Sleeper Smith <jason.chen@bit-clouded.com>
2014-04-15 19:56:21 +1000

    .

--- src/Tools/init.ps1
+++ src/Tools/init.ps1
@@ -1,3 +1,20 @@
 param($installPath, $toolsPath, $package, $project)
 
 Copy-Item "$toolsPath\kaiseki-bootstrap\" -Destination "$installPath\..\.." -Force
+
+<#
+# Get the open solution.
+$solution = Get-Interface $dte.Solution ([EnvDTE80.Solution2])
+
+# Create the parent solution folder.
+$parentProject = $solution.AddSolutionFolder("Parent")
+
+# Create a child solution folder.
+$parentSolutionFolder = Get-Interface $parentProject.Object ([EnvDTE80.SolutionFolder])
+$childProject = $parentSolutionFolder.AddSolutionFolder("Child")
+
+# Add a file to the child solution folder.
+$childSolutionFolder = Get-Interface $childProject.Object ([EnvDTE80.SolutionFolder])
+$fileName = "D:\projects\MyProject\MyProject.csproj"
+$projectFile = $childSolutionFolder.AddFromFile($fileName)
+#>
--- src/Tools/kaiseki-bootstrap/build.ps1
+++ src/Tools/kaiseki-bootstrap/build.ps1
@@ -1,1 +1,3 @@
 .".\.nuget\NuGet.exe" restore .\Sample.sln
+
+.\packages\psake.4.3.2\tools\psake.ps1 -taskList Invoke-MsBuild

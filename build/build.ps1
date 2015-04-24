e4b5dd192944a28c28dacd3210fbb22547accdfd
Jason Chen <jason.chen@bit-clouded.com>
2015-04-24 21:35:20 +1000

    Transform the kaiseki-bootstrap/build.ps1 to get the version number from the package building.

---
+++ build/build.ps1
@@ -0,0 +1,17 @@
+# build transform
+$specFileOriginal = Get-ChildItem -Filter *.bt.* -Recurse -File
+
+$specFileOriginal | % {
+	#'.\src\Kaiseki.bt.nuspec'
+    $oldFilePath = Resolve-Path $_.FullName
+    $specFileOriginalContent = [system.io.file]::ReadAllText($oldFilePath.ProviderPath)
+    $specFileOriginalContent = $specFileOriginalContent.Replace('#version#', '1.0.1')
+
+    $specFile = $oldFilePath.ProviderPath.Replace(".bt.", ".") #'.\src\Kaiseki.nuspec'
+    Set-Content -Path $specFile -Value $specFileOriginalContent
+    Remove-Item $_.FullName -Force
+}
+
+
+# pack
+."build\Nuget.exe" pack .\src\Kaiseki.nuspec
--- src/Tools/kaiseki-bootstrap/build.bt.ps1
+++ src/Tools/kaiseki-bootstrap/build.bt.ps1
@@ -7,7 +7,7 @@ Write-Host "== Restoring packages =="
 .".\.nuget\NuGet.exe" restore
 
 Write-Host "== Starting psake build =="
-.\packages\psake.4.3.2\tools\psake.ps1 .\packages\Kaiseki.1.0.0\tools\Load-Modules.ps1
+.\packages\psake.4.3.2\tools\psake.ps1 .\packages\Kaiseki.#version#\tools\Load-Modules.ps1
 
 Write-Host
 Write-Host "######################"
